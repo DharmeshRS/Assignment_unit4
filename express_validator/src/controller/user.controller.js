0d5e3e3c06cafd13693e7a607279d96ae484fedf
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2021-12-06 09:31:37 +0530

    express validator

---
+++ express_validator/src/controller/user.controller.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const User = require("../models/user.model");
+const {body,validationResult}=require("express-validator")
+const router=express.Router();
+
+router.post("/",
+    body("id").isLength({min:1}).withMessage("Id is Required"),
+    body("first_name").isLength({min:1}).withMessage("first name is required and must be valid"),
+    body("last_name").isLength({min:1}).withMessage("Last name is required and must be valid"),
+    body("email").isEmail().withMessage("Email Invalid format"),
+    body("gender").isLength({min:1}).withMessage("Gender Is Invalid"),
+    body("age").isLength({min:1}).withMessage("Age is 18+"),  
+
+    async(req,res)=>{
+
+        const errors=validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({data:errors.array()})
+        }
+        const user=await User.create(req.body)
+        return res.status(201).json({data:user})
+});
+
+
+module.exports=router;
---
+++ express_validator/src/server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+
+const app=express()
+const port=8000
+const connect=require("./configs/db")
+const userController=require("./controller/user.controller")
+app.use(express.json())
+app.use("/users",userController)
+const start=async ()=>{
+        await connect();
+
+        app.listen(port,()=>{
+            console.log(`listening on port ${port}`)
+        })
+}
+
+module.exports=start;
+
