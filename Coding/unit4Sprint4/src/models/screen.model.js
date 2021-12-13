b17848ebb8bd291d4f1bd0f2ce7696fe89615b7d
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2021-12-13 14:29:57 +0530

    evaluation update

---
+++ Coding/unit4Sprint4/src/models/screen.model.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const theatreModel = require('./theatre.model')
+
+const screenSchema=new mongoose.Schema({
+    name:{
+        type:String
+    },
+    theatre_id:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:theatreModel
+    }]
+})
---
+++ Coding/unit4Sprint4/src/models/theatre.model.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+
+const theatreSchema=new mongoose.Schema({
+    name:{
+        type:String
+    },
+    location:{
+        type:String
+    }
+})
+
+module.exports=mongoose.Model('theatre',theatreSchema)
