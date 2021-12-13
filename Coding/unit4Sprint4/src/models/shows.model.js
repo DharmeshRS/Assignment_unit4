8eb35ae9d1854dea11f4c903d387c770c58671d8
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2021-12-13 14:33:22 +0530

    Update shows.model.js

--- Coding/unit4Sprint4/src/models/shows.model.js
+++ Coding/unit4Sprint4/src/models/shows.model.js
@@ -1,5 +1,13 @@
 const mongoose=require('mongoose')
-
+const movieSchema=require('./movie.model')
 const showSchema=mongoose.Schema({
-    
+    timing:{
+        type:String
+    },
+    movie_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:movieSchema
+    }
 })
+
+module.exports=mongoose.Model('show',showSchema)
