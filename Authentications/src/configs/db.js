5adc87bbfe0f9d7339f033c1d2113971324d8760
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2021-12-13 22:06:55 +0530

    authenication assignment

--- Authentications/src/configs/db.js
+++ Authentications/src/configs/db.js
@@ -1,15 +1,5 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connect=()=>{
-    return mongoose.connect("mongodb://localhost:27017/authentication")
-    // ,{
-    //     useNewUrlParser:true,
-    //     useUnifiedTopology:true,
-    //     useCreateIndex:true,
-    //     useFindAndModify:true
-    // });;
-
+module.exports = () => {
+  return mongoose.connect("mongodb://localhost:27017/web12-authentication");
 };
-
-
-module.exports=connect;
--- Authentications/src/server.js
+++ Authentications/src/server.js
@@ -1,23 +1,7 @@
-const mongoose=require('mongoose')
-const express=require('express')
-const { connect } = require('./configs/db')
-const { register,login } = require('./controllers/user.controller')
+const app = require("./index");
+const connect = require("./configs/db");
 
-const port=process.env.port || 8000
-
-const app=express()
-app.use(express.json())
-app.post("/register",register);
-app.post("/login",login)
-
-const start=async ()=>{
-    const con=await connect()
-    console.log("connection Established...");
-
-    app.listen(port,()=>{
-        console.log(`Listening On port ${port}`)
-    })
-}
-
-module.exports=start;
-
+app.listen(2345, async function () {
+  await connect();
+  console.log("listening on port 2345");
+});
